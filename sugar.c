a61860a091b5b4113befdfd06a6ad0e7c07fe2d5
zzangminP <38036544+zzangminP@users.noreply.github.com>
2018-05-12 19:56:14 +0900

    백준 2839번 답

---
+++ sugar.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void) {
+	int n, x;
+	scanf("%d", &n);
+	switch (n)
+	{
+	case 0:
+		printf("0");
+		break;
+	case 1:
+		printf("-1");
+		break;
+	case 2:
+		printf("-1");
+		break;
+	case 4:
+		printf("-1"); 
+		break;
+	case 7:
+		printf("-1");
+		break;
+	default:
+
+		if ((n % 5 == 0)) {
+			x = n / 5;
+			printf("%d", x);
+		}
+		else if ((n % 5 == 1) || (n % 5 == 3)) {
+			x = n / 5 + 1;
+			printf("%d", x);
+		}
+		else if ((n % 5 == 2) || (n % 5 == 4)) {
+			x = n / 5 + 2;
+			printf("%d", x);
+		}
+
+		break;
+	}
+
+}
