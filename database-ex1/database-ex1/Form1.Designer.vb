9fbf0425cce7c73f78201fb7b24484502042cc5a
zzangminP <38036544+zzangminP@users.noreply.github.com>
2018-06-12 15:13:36 +0900

    0612 Visual basic - Database

---
+++ database-ex1/database-ex1/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me._학생정보_ndbDataSet = New database_ex1._학생정보_ndbDataSet()
+        Me.테이블1BindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.테이블1TableAdapter = New database_ex1._학생정보_ndbDataSetTableAdapters.테이블1TableAdapter()
+        CType(Me._학생정보_ndbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.테이블1BindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(13, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "학번"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(14, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(29, 12)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "이름"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(15, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 12)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "주소"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(15, 158)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "연락처"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.테이블1BindingSource, "학번", True))
+        Me.TextBox1.Location = New System.Drawing.Point(88, 36)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.테이블1BindingSource, "이름", True))
+        Me.TextBox2.Location = New System.Drawing.Point(88, 76)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 21)
+        Me.TextBox2.TabIndex = 5
+        '
+        'TextBox3
+        '
+        Me.TextBox3.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.테이블1BindingSource, "주소", True))
+        Me.TextBox3.Location = New System.Drawing.Point(88, 114)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(218, 21)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.DataBindings.Add(New System.Windows.Forms.Binding("Text", Me.테이블1BindingSource, "연락처", True))
+        Me.TextBox4.Location = New System.Drawing.Point(88, 158)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(291, 21)
+        Me.TextBox4.TabIndex = 7
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 241)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "처음"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(122, 241)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "이전"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(231, 241)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "다음"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(343, 241)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "마지막"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        '_학생정보_ndbDataSet
+        '
+        Me._학생정보_ndbDataSet.DataSetName = "_학생정보_ndbDataSet"
+        Me._학생정보_ndbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        '테이블1BindingSource
+        '
+        Me.테이블1BindingSource.DataMember = "테이블1"
+        Me.테이블1BindingSource.DataSource = Me._학생정보_ndbDataSet
+        '
+        '테이블1TableAdapter
+        '
+        Me.테이블1TableAdapter.ClearBeforeFill = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 319)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me._학생정보_ndbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.테이블1BindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents _학생정보_ndbDataSet As 학생정보DataSet
+    Friend WithEvents 테이블1BindingSource As BindingSource
+    Friend WithEvents 테이블1TableAdapter As _학생정보_ndbDataSetTableAdapters.테이블1TableAdapter
+End Class
---
+++ database-ex1/database-ex1/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: 이 코드는 데이터를 '학생정보DataSet.테이블1' 테이블에 로드합니다. 필요 시 이 코드를 이동하거나 제거할 수 있습니다.
+        Me.테이블1TableAdapter.Fill(Me._학생정보_ndbDataSet.테이블1)
+        'TODO: 이 코드는 데이터를 '학생정보DataSet.테이블1' 테이블에 로드합니다. 필요 시 이 코드를 이동하거나 제거할 수 있습니다.
+        Me.테이블1TableAdapter.Fill(Me._학생정보_ndbDataSet.테이블1)
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rowcount As Integer
+        rowcount = Me.BindingContext(테이블1BindingSource).Count
+        If rowcount > 0 Then
+            Me.테이블1BindingSource.Position = 0
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim rowcount As Integer
+        rowcount = Me.BindingContext(테이블1BindingSource).Count
+        If rowcount > 0 Then
+            Me.테이블1BindingSource.Position = Me.테이블1BindingSource.Position - 1
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim rowcount As Integer
+        rowcount = Me.BindingContext(테이블1BindingSource).Count
+        If rowcount > Me.테이블1BindingSource.Position Then
+            Me.테이블1BindingSource.Position = Me.테이블1BindingSource.Position + 1
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim rowcount As Integer
+        rowcount = Me.BindingContext(테이블1BindingSource).Count
+        If rowcount > 0 Then
+            Me.테이블1BindingSource.Position = rowcount - 1
+        End If
+    End Sub
+End Class
